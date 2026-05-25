0da13846618a6add3b5eb71225294d243c9e8b5a
vin3912 <vinivini3912@naver.com>
2026-05-25 17:03:58 +0900

    fix: Home.jsx 헤더 중복 제거

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,23 +1,19 @@
-import Header from '../component/Common/Header/Header'
 import HeroBanner from '../component/Home/HeroBanner/HeroBanner'
 import DayTrip from '../component/Home/DayTrip/DayTrip'
 import CourseBuilder from '../component/Home/CourseBuilder/CourseBuilder'
 import RegionGrid from '../component/Home/RegionGrid/RegionGrid'
 import Trending from '../component/Home/Trending/Trending'
 
-function App() {
+function Home() {        
   return (
-    <>
-      <Header />
-      <main>
-        <HeroBanner />
-        <DayTrip />
-        <CourseBuilder />
-        <RegionGrid />
-        <Trending />
-      </main>
-    </>
+    <main>
+      <HeroBanner />
+      <DayTrip />
+      <CourseBuilder />
+      <RegionGrid />
+      <Trending />
+    </main>
   )
 }
 
-export default App
+export default App       
