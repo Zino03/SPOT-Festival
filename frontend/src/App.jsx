89bf89c36ddf55f5c851702141fc3d7c56a6adb0
vin3912 <vinivini3912@naver.com>
2026-05-31 19:12:24 +0900

    style: App.jsx 라우터 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import MapPage from './pages/MapPage'
 import RegionPage from './pages/RegionPage'
-import FestivalDetailPage from './pages/FestivalDetailPage'
 import Header from './component/Common/Header/Header'
+import FestivalPage from './pages/FestivalPage'
+import SpotComparePage from './pages/SpotComparePage'
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/map" element={<MapPage />} />
         <Route path="/region/:regionId" element={<RegionPage />} />
-        <Route path="/festival/:id" element={<FestivalDetailPage />} />
+        <Route path="/festival/:festivalId" element={<FestivalPage />} />
+        <Route path="/festival/:festivalId/compare" element={<SpotComparePage />} />
       </Routes>
     </BrowserRouter>
   )
