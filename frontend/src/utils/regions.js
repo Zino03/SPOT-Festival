414a02be4235ea649994a154b69f723285fae654
Zino03 <zldnl454@naver.com>
2026-06-01 22:47:35 +0900

    refactor: 프론트 정리 및 카카오 지도 개선

---
+++ frontend/src/utils/regions.js
@@ -0,0 +1,6 @@
+export const REGION_NAMES = {
+  seoul: '서울', gyeonggi: '경기', incheon: '인천', gangwon: '강원',
+  chungnam: '충남', chungbuk: '충북', daejeon: '대전', sejong: '세종',
+  jeonbuk: '전북', jeonnam: '전남', gwangju: '광주', gyeongbuk: '경북',
+  gyeongnam: '경남', daegu: '대구', ulsan: '울산', busan: '부산', jeju: '제주',
+}
