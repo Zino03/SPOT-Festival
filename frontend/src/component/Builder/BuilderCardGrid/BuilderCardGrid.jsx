52d4485ba82fc0fa0e1eec83f5bfb4bfc50d4338
sang <sang3141592@gmail.com>
2026-06-02 17:52:54 +0900

    refacotr: 더미내용들 제거

--- frontend/src/component/Builder/BuilderCardGrid/BuilderCardGrid.jsx
+++ frontend/src/component/Builder/BuilderCardGrid/BuilderCardGrid.jsx
@@ -2,27 +2,6 @@ import { useState, useEffect } from 'react'
 import { loadKakaoMap } from '../../../utils/kakao'
 import './BuilderCardGrid.css'
 
-// 단계별 더미 데이터 (step 1은 API로 교체됨)
-const DUMMY_ITEMS = {
-  2: [
-    { id: 1, name: 'OO국밥',    category: '한식', price: '₩8,000~',  rating: 4.8, distance: '0.4km', walk: '12분', wait: null,  image: '', isAI: true,  isSelected: true  },
-    { id: 2, name: '△△스테이크', category: '양식', price: '₩12,000~', rating: 4.4, distance: '0.7km', walk: '8분',  wait: '5분', image: '', isAI: false, isSelected: false },
-    { id: 3, name: 'OO파스타',  category: '양식', price: '₩14,000~', rating: 4.5, distance: '0.9km', walk: '11분', wait: '0분', image: '', isAI: false, isSelected: false },
-    { id: 4, name: '▽▽분식',   category: '분식', price: '₩6,000~',  rating: 4.3, distance: '0.5km', walk: '7분',  wait: '8분', image: '', isAI: false, isSelected: false },
-    { id: 5, name: '◇◆중식',   category: '한식', price: '₩9,500~',  rating: 4.2, distance: '0.8km', walk: '10분', wait: '15분',image: '', isAI: false, isSelected: false },
-  ],
-  3: [
-    { id: 1, name: '한적 카페',   category: '카페', price: '₩5,000~', rating: 4.7, distance: '0.3km', walk: '4분',  wait: null,  image: '', isAI: true,  isSelected: false },
-    { id: 2, name: '감성 베이커리', category: '베이커리', price: '₩4,000~', rating: 4.4, distance: '0.6km', walk: '8분', wait: null, image: '', isAI: false, isSelected: false },
-    { id: 3, name: '루프탑 카페', category: '카페', price: '₩6,000~', rating: 4.3, distance: '0.9km', walk: '11분', wait: '3분', image: '', isAI: false, isSelected: false },
-  ],
-  4: [
-    { id: 1, name: '한강 공영주차장', category: '무료', price: 'Free',    rating: 4.5, distance: '0.2km', walk: '2분',  wait: null,  image: '', isAI: true,  isSelected: false },
-    { id: 2, name: '충북 중앙 P타워', category: '유료', price: '₩2,000/h', rating: 4.7, distance: '0.3km', walk: '4분', wait: null,  image: '', isAI: false, isSelected: false },
-    { id: 3, name: '지하 P동',       category: '유료', price: '₩1,500/h', rating: 4.4, distance: '0.4km', walk: '5분', wait: null,  image: '', isAI: false, isSelected: false },
-  ],
-}
-
 const KAKAO_CODE = { 2: 'FD6', 3: 'CE7' }
 const SHOW_COUNT = 5
 
